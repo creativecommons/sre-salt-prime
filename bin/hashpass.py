256c2aca2f49ce46656f812c9ae439c669e2d7a7
Timid Robot Zehta <timid@creativecommons.org>
2018-12-07 21:28:23 +0000

    moved hashpass.py to bin and updated to work from there

--- bin/hashpass.py
+++ bin/hashpass.py
@@ -7,18 +7,23 @@
 # Standard library
 import crypt
 import getpass
+import os
+import inspect
 
 # Third-party
 import ruamel.yaml
 
 
-newpass = getpass.getpass("Enter new password: ")
 username = getpass.getuser()
-passpath = "stacks/01-common/user_passwords/{}.yaml".format(username)
+path_script = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
+path_repo = os.path.dirname(path_script)
+path_passwords = os.path.join(path_repo, "stacks", "01-common",
+                              "user_passwords", "{}.yaml".format(username))
+newpass = getpass.getpass("Enter new password: ")
 if newpass == getpass.getpass("Retype new password: "):
     hashedpass = crypt.crypt(newpass, crypt.METHOD_SHA512)
     data = {"user": {"passwords": {username: hashedpass}}}
-    with open(passpath, "w") as passfile:
+    with open(path_passwords, "w") as passfile:
         ruamel.yaml.dump(data, passfile, default_flow_style=False)
     print()
     print("REMINDER: you still need to update hosts and commit changes.")
