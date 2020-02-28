4241138f3e7be321e258908801ad41f0fa859355
Timid Robot Zehta <timid@creativecommons.org>
2020-02-28 16:11:04 +0000

    updated to use a Heredoc multiline block

--- states/nginx/files/updated_cloudlfare_ips.sh
+++ states/nginx/files/updated_cloudlfare_ips.sh
@@ -15,38 +15,36 @@ IPV6="$(curl --http2 --raw --silent https://www.cloudflare.com/ips-v6 \
         | sort \
         | ipv6calc --addr_to_compressed)"
 
-{
-    echo '# Managed by SaltStack: {{ SLS }}'
-    echo '#'
-    printf '# To update this file, run the updated_cloudlfare_ips.sh script'
-    printf ' on Salt-Prime\n# and then deploy via SaltStack\n'
-    echo
-    echo
-    echo '# Restoring original visitor IPs: Logging visitor IP addresses with'
-    echo '# mod_cloudflare – Cloudflare Support'
-    printf '# https://support.cloudflare.com/hc/en-us/articles/'
-    printf '200170786-Restoring-original-visitor-IPs-Logging-visitor-IP-'
-    printf 'addresses-with-mod-cloudflare-\n'
-    echo
-    echo
-    echo '# https://www.cloudflare.com/ips-v4'
-    for _ip in ${IPV4}
+cat << HEREDOC > real_ip_from_cloudflare.conf
+# Managed by SaltStack: {{ SLS }}
+#
+# To update this file, run the updated_cloudlfare_ips.sh script on Salt-Prime
+# and then deploy via SaltStack
+
+
+# Restoring original visitor IPs: Logging visitor IP addresses with
+# mod_cloudflare – Cloudflare Support
+# https://support.cloudflare.com/hc/en-us/articles/200170786-Restoring-original-visitor-IPs-Logging-visitor-IP-addresses-with-mod-cloudflare-
+
+
+# https://www.cloudflare.com/ips-v4
+$(  for _ip in ${IPV4}
     do
         echo "set_real_ip_from ${_ip};"
-    done
-    echo
-    echo
-    echo '# https://www.cloudflare.com/ips-v6'
-    for _ip in ${IPV6}
+    done)
+
+
+# https://www.cloudflare.com/ips-v6
+$(  for _ip in ${IPV6}
     do
         echo "set_real_ip_from ${_ip};"
-    done
-    echo
-    echo
-    echo 'real_ip_header X-Forwarded-For;'
-    echo
-    echo
-    echo '# vim: ft=nginx:'
-} > real_ip_from_cloudflare.conf
+    done)
+
+
+real_ip_header X-Forwarded-For;
+
+
+# vim: ft=nginx:
+HEREDOC
 
 # vim: ft=sh:
