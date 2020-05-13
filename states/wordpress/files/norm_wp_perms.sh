c4ad856ee61fa1330d2ab98e9af12bf817b826d8
Timid Robot Zehta <timid@creativecommons.org>
2020-05-13 15:50:34 +0000

    added composer.json, wp-content/wflogs. remove setgid
    - added group/mode management of composer.json
    - fixed removal of setgid
    - updated user of wp-content/uploads
    - added user/group/mode management of wp-content/wflogs

--- states/wordpress/files/norm_wp_perms.sh
+++ states/wordpress/files/norm_wp_perms.sh
@@ -6,6 +6,10 @@
 # - Maximize security / minimize risk
 # - Allow web development without requiring root privilages
 #
+# References
+# - https://wordpress.org/support/article/changing-file-permissions/
+# - https://www.gnu.org/software/coreutils/manual/html_node/Directory-Setuid-and-Setgid.html
+#
 set -o errexit
 set -o errtrace
 set -o nounset
@@ -40,6 +44,13 @@ WP_CONFIGS=$(find /var/www -maxdepth 2 -type f -name 'wp-config.php' \
 for _wp_config in ${WP_CONFIGS}; do
     WP_DIR="${_wp_config%/*}"
     headerone 'Normalzing permissions on WordPress top directory:'
+    # composer.json file
+    TARGET="${WP_DIR}/composer.json"
+    headertwo "${TARGET}"
+    if [[ -f "${TARGET}" ]]; then
+        chgrp -ch webdev "${TARGET}"
+        chmod -c 00664 "${TARGET}"
+    fi
     # backup directory
     TARGET="${WP_DIR}/backup"
     headertwo "${TARGET}"
@@ -52,7 +63,7 @@ for _wp_config in ${WP_CONFIGS}; do
             -o -type f -exec chmod -c 0660 {} + \
             | sed -e'/retained as/d'
     fi
-    # vendor symlink
+    # vendor symlink - unnecessary aesthetics ¯\_(ツ)_/¯
     TARGET="${WP_DIR}/vendor"
     headertwo "$(printf '%-43s (symlink)\n' "${TARGET}")"
     if [[ -L "${TARGET}" ]]; then
@@ -74,22 +85,32 @@ for _wp_config in ${WP_CONFIGS}; do
     headertwo "${TARGET}"
     if [[ -d "${TARGET}" ]]; then
         chown -ch root:root "${TARGET}"
-        # Why 00555 instead of 0555? See:
-        # https://www.gnu.org/software/coreutils/manual/html_node/Directory-Setuid-and-Setgid.html
         chmod -c 00555 "${TARGET}"
         # wp-content subdirectories
         for _dir in $(/usr/bin/find "${TARGET}" -maxdepth 1 -type d); do
+            TARGET_USER=composer
+            TARGET_GROUP=webdev
+            TARGET_FMODE=00664
             case ${_dir} in
-                "${TARGET}")    continue;;
-                *uploads)       TARGET_GROUP=www-data;;
-                *)              TARGET_GROUP=webdev;;
+                "${TARGET}")
+                    continue
+                    ;;
+                *uploads)
+                    TARGET_USER=www-data
+                    TARGET_GROUP=www-data
+                    ;;
+                *wflogs)
+                    TARGET_USER=www-data
+                    TARGET_GROUP=www-data
+                    TARGET_FMODE=00660
+                    ;;
             esac
             headertwo "$(printf '%-43s group: %s\n' "${_dir}" \
                             "${TARGET_GROUP}")"
-            chown -chR composer:"${TARGET_GROUP}" "${_dir}"
+            chown -chR "${TARGET_USER}":"${TARGET_GROUP}" "${_dir}"
             /usr/bin/find "${_dir}" \
                 -type d -exec chmod -c 2775 {} + \
-                -o -type f -exec chmod -c 0664 {} +
+                -o -type f -exec chmod -c "${TARGET_FMODE}" {} +
         done
     fi
 done
