364ab424f5e935599508ab0288337e490882d3bd
Timid Robot Zehta <timid@creativecommons.org>
2020-06-12 16:05:47 +0000

    added checking to prevent warnings on non-sheltered sites

--- states/wordpress/files/composer-wp-config.php
+++ states/wordpress/files/composer-wp-config.php
@@ -10,7 +10,7 @@
 if ( !defined('ABSPATH') ) {
     define('ABSPATH', dirname(__FILE__) . '/');
 }
-if($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'){
+if ( isset ( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
     $_SERVER['HTTPS']       = 'on';
     $_SERVER['SERVER_PORT'] = 443;
 }
